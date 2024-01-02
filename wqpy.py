d8ceb39be29f3250cf8daf069463f7f933f5aefb
Jacob Brown <brown.jacob.alexander@gmail.com>
2024-01-02 13:43:14 -0700

    Organized file structure

--- wqpy.py
+++ wqpy.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import Union
 import requests
 import json
+from lookup_tables import state_letters_to_state_code, nm_county_name_to_county_code
 
 SITE_URL = 'https://www.waterqualitydata.us/data/Station/search?'
 RESULTS_URL = 'https://www.waterqualitydata.us/data/Result/search?'
@@ -17,95 +18,6 @@ SUMMARY_DATA_URL = 'https://www.waterqualitydata.us/data/summary/monitoringLocat
 # END TYPE HINTING
 # ------------------
 
-state_letters_to_statecode = {
-    'AL': 'US:01',
-    'AK': 'US:02',
-    'AZ': 'US:03',
-    'AR': 'US:04',
-    'CA': 'US:05',
-    'CO': 'US:08',
-    'CT': 'US:09',
-    'DE': 'US:10',
-    'DC': 'US:11',
-    'FL': 'US:12',
-    'GA': 'US:13',
-    'HW': 'US:15',
-    'ID': 'US:16',
-    'IL': 'US:17',
-    'IN': 'US:18',
-    'IA': 'US:19',
-    'KS': 'US:20',
-    'KY': 'US:21',
-    'LA': 'US:22',
-    'ME': 'US:23',
-    'MD': 'US:24',
-    'MA': 'US:25',
-    'MI': 'US:26',
-    'MN': 'US:27',
-    'MS': 'US:28',
-    'MO': 'US:29',
-    'MT': 'US:30',
-    'NE': 'US:31',
-    'NV': 'US:32',
-    'NH': 'US:33',
-    'NJ': 'US:34',
-    'NM': 'US:35',
-    'NY': 'US:36',
-    'NC': 'US:37',
-    'ND': 'US:38',
-    'OH': 'US:39',
-    'OK': 'US:40',
-    'OR': 'US:41',
-    'PA': 'US:42',
-    'RI': 'US:44',
-    'SC': 'US:45',
-    'SD': 'US:46',
-    'TN': 'US:47',
-    'TX': 'US:48',
-    'UT': 'US:49',
-    'VT': 'US:50',
-    'VA': 'US:51',
-    'WA': 'US:53',
-    'WV': 'US:54',
-    'WI': 'US:55',
-    'WY': 'US:56'
-}
-
-county_name_to_code = {
-    'bernalillo': 'US:35:001',
-    'catron': 'US:35:003',
-    'chaves': 'US:35:005',
-    'cibola': 'US:35:006',
-    'colfax': 'US:35:007',
-    'curry': 'US:35:009',
-    'debaca': 'US:35:011',
-    'dona ana': 'US:35:013',
-    'eddy': 'US:35:015',
-    'grant': 'US:35:017',
-    'guadalupe': 'US:35:019',
-    'harding': 'US:35:021',
-    'hidalgo': 'US:35:023',
-    'lea': 'US:35:025',
-    'lincoln': 'US:35:027',
-    'los alamos': 'US:35:028',
-    'luna': 'US:35:029',
-    'mckinley': 'US:35:031',
-    'mora': 'US:35:033',
-    'otero': 'US:35:035',
-    'quay': 'US:35:037',
-    'rio arriba': 'US:35:039',
-    'roosevelt': 'US:35:041',
-    'sandoval': 'US:35:043',
-    'san juan': 'US:35:045',
-    'san miguel': 'US:35:047',
-    'santa fe': 'US:35:049',
-    'sierra': 'US:35:051',
-    'socorro': 'US:35:053',
-    'taos': 'US:35:055',
-    'torrance': 'US:35:057',
-    'union': 'US:35:059',
-    'valencia': 'US:35:061'
-}
 
 def create_query_list(s: Union[list, str]) -> list:
     '''
@@ -169,13 +81,13 @@ def query_wqp(url: str,
     if stateletters:
         stateletters = create_query_list(stateletters)
         stateletters = [letters.upper() for letters in stateletters]
-        statecodes = [state_letters_to_statecode[sl] for sl in stateletters]
+        statecodes = [state_letters_to_state_code[sl] for sl in stateletters]
         query_params["statecode"] = statecodes
 
     if countyname:
         countynames = create_query_list(countyname)
         countynames = [name.lower() for name in countynames]
-        countynames = [county_name_to_code[name] for name in countynames]
+        countynames = [nm_county_name_to_code[name] for name in countynames]
         query_params['countycode'] = countynames
 
     if siteType:
